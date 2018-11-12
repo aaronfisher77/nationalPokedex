844bf2e2aefb7f8a583b36d7d8ca3c9c47a6b2f8
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-11-12 10:20:06 -0600

    Initial Commit

---
+++ nationalPokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nationalPokedex
+//
+//  Created by Aaron Fisher on 11/12/18.
+//  Copyright © 2018 Aaron Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
