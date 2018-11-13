d8892105e495f762f5cba9bbb125ace4607726f5
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-11-13 12:28:36 -0600

    Created a Error message that notifies when the entered string is not a pokemon.

--- nationalPokedex/ViewController.swift
+++ nationalPokedex/ViewController.swift
@@ -13,9 +13,16 @@ import Alamofire
 class ViewController: UIViewController {
     
     @IBOutlet weak var nameOrIDTextView: UITextField!
-    @IBOutlet weak var infoTextView: UITextView!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var pokemonUIImageView: UIImageView!
+    
+    @IBOutlet weak var errorLabel: UITextView!
     
     let pokedexAPIBaseURL = "https://pokeapi.co/api/v2/pokemon/"
+    
+    let pokemonImageURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +34,17 @@ class ViewController: UIViewController {
         //Dismissing Keyboared
         nameOrIDTextView.resignFirstResponder()
         
-        // Checking to make sure both fields have values
+        // Checking to make sure what was entered is a Pokemon or pokemon ID
         guard let pokemonName = nameOrIDTextView.text else {
                 return
         }
         
-        // Clearing out text field
+        // Clears out text field
         nameOrIDTextView.text = ""
+        
+        //Replacing spaces in the name/title with + so they can be used as a part of the URL
+        let pokemonURLComponant = pokemonName.replacingOccurrences(of: " ", with: "+")
+        
         
         //Building our complete request url with name/song
         let requestURL = pokedexAPIBaseURL + pokemonName
@@ -41,10 +52,19 @@ class ViewController: UIViewController {
         Alamofire.request(requestURL).responseJSON { (response) in
             switch response.result {
             case .success(let value):
+        
                 let json = JSON(value)
-                self.infoTextView.text = String(describing: json)
+
+                
+                
+                
+                
+                
+                
+                
+                
             case .failure(let error):
-                self.infoTextView.text = "Invalid Selection entered or an error occured. Please try again!"
+                self.nameLabel.text = "Invalid Selection please enter a valid Pokemon."
                 print(error.localizedDescription)
             }
         }
